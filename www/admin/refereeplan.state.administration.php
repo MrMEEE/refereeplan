c5675aaff9aea700f734f1356548d3411987b756
MrMEEE <mj@casalogic.dk>
2013-11-29 00:56:18 +0100

    Ajax for Configuration...

--- www/admin/refereeplan.state.administration.php
+++ www/admin/refereeplan.state.administration.php
@@ -5,6 +5,13 @@ case "configuration":
   list ($clubs,$ids) = getClubs();
   
   $clubids = getClubIDs();
+  
+  foreach($clubids as $id){
+  	$clubslist .= '"'.$id.'",';
+  }
+  
+  $clubslist = substr($clubslist, 0, -1);
+  
   for ($k = 0; $k < count($clubs); $k++){
 	$clubnames[$ids[$k]]=$clubs[$k];
 	$clublist.= "<option value=\"".$ids[$k].":".fixCharacters($clubs[$k])."\">".fixCharacters($clubs[$k])."</option>";
@@ -12,7 +19,7 @@ case "configuration":
 
   $javascript .= 'function addSisterClubs(select){
 			var id = Math.floor( Math.random()*99999 );
-			$("#sisterClubs").append(\'<select id="sisterClub-\'+id+\'" name="clubselect[]" onchange="javascript:changeClubs()">'.addslashes($clublist).'</select><br>\');
+			$("#sisterClubs").append(\'<select id="sisterClub-\'+id+\'" name="clubselect[]" onchange="javascript:changeClubs()">'.addslashes($clublist).'</select>\');
 			$("#sisterClub-"+id+\' option[value^="\'+select+\':"]\').attr("selected", true);
 		}
 		  
@@ -21,11 +28,34 @@ case "configuration":
 			$.post("ajax/refereeplan.ajax.administration.php", $("#mainForm").serialize());
 			document.mainForm.changeClub.value="";
 			$("#message").text("'.fetchText("Settings were updated").'").show().fadeOut(1000);
+			var inputs = document.getElementById("sisterClubs").childNodes;
+			//var inputs = $("#sisterClubs select");
+			for (x=0;x<=inputs.length;x++){
+				    //alert(inputs[x].getAttribute("id"));
+				    var ti=inputs[x].selectedIndex;
+				    var op=inputs[x].options;
+				    //alert(op[ti].value);
+				    if(op[ti].value == ":"){
+					  $("#"+inputs[x].getAttribute("id")).remove();
+				    }
+			}
+			
 		  }
 		  
 		  function changeUpdatesUrl(){
 			document.mainForm.submit();
-		  }';
+		  }
+		  
+		  function generateClubs(){
+			var clubs=['.$clubslist.'];
+			for (var i=0,len=clubs.length; i<len; i++){
+				addSisterClubs(clubs[i]);		
+			}
+		  }
+		  $(document).ready(function() {
+			generateClubs();
+		  });
+		  ';
   
   echo fetchText("Configuration","header2");
   
@@ -42,9 +72,22 @@ case "configuration":
       }
   }
   
-  echo '<div id="message"></div>';
+
+  
+  echo '<table width="100%">
+	  <tr>
+	    <td width="50%">';
   
   echo fetchText("Select Club:","header3");
+  
+  echo '</td>
+	<td width="50%" align="right">';
+	
+  echo '<div id="message"></div>';
+  
+  echo '</td>
+	</tr>
+	</table>';
   
   echo '<select name="clubselect[]" onchange="changeClubs();">'.
           $clublist
@@ -52,29 +95,7 @@ case "configuration":
   
   echo fetchText("Sister Clubs:","header3");
   
-  echo '<div id="sisterClubs">';
-  for ($i = 1; $i <= count($clubids)-1; $i++){
-    if($clubids[$i]==""){
-      $clublist = "<option value=\":\" selected>".fetchText("No Club selected/Remove Sister Club")."</option>";
-    }else{
-      $clublist = "<option value=\":\">".fetchText("No Club selected/Remove Sister Club")."</option>";
-    }
-   
-    for ($k = 0; $k < count($clubs); $k++){
-      if($ids[$k]==$clubids[$i]){
-	$clublist.= "<option value=\"".$ids[$k].":".fixCharacters($clubs[$k])."\" selected>".fixCharacters($clubs[$k])."</option>";
-      }else{
-	$clublist.= "<option value=\"".$ids[$k].":".fixCharacters($clubs[$k])."\">".fixCharacters($clubs[$k])."</option>";
-      }
-    }
-
-    echo '<select name="clubselect[]" onchange="javascript:changeClubs()">
-	   '.$clublist.' 
-	  </select><br>';
-  }
-  
-  echo '</div>';
-  
+  echo '<div id="sisterClubs"></div>';
   echo '<br><a href="#" onclick="javascript:addSisterClubs(0);"><img width="15px" src="img/add.png">'.fetchText("Add Sister Club").'</a><br>';
   
   echo fetchText("URL for Updates:","header3");
